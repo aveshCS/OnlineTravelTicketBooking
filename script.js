4066c5d3ad03d4354617bc6b092281a206eeabfd
Avesh Singh <aveshzoop@gmail.com>
2022-04-13 22:03:27 +0530

    Added Java Script

---
+++ script.js
@@ -0,0 +1,22 @@
+// let heading=document.getElementById("containerHeading");
+// heading.innerHTML="Search Your PNR here";
+// let trainSearchButton=document.getElementById("trainSearchButton");
+//Train Ticket booking
+function booked(){
+    alert('Your ticket has been booked! Happy Journey :)');
+}
+function trainPnrSearch(){
+    alert('We have mailed you your PNR details')
+}
+//Flight tickets
+function flightTicketBooked(){
+    alert('Your flight has been booked! Happy Flying :)')
+}
+function flightPnrSearch(){
+    alert('We have mailed you your PNR details')
+}
+
+//Contact Us
+function messageReceived(){
+    alert('Thank You for contacting us')
+}
