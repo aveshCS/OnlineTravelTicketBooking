fbe17f771a4545b4b52498e14ee5cd5469e2f553
Avesh Singh <aveshzoop@gmail.com>
2022-04-14 14:39:31 +0530

    JavaScript puntuations

--- script.js
+++ script.js
@@ -12,8 +12,8 @@
 function booked() {
     let email = prompt('Please enter your Email address')
     if (email) {
-        alert('We have mailed you the cheapest train ticket fares for the opted route')
-      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+        alert('We have mailed you, the cheapest train ticket fares for the opted route')
+      let deal = prompt('Please enter 1, to book you the best deal out of all of them')
       if(deal==1){
           alert('Your Ticket has been booked! Thank You for choosing us: Enjoy your Journey :)');
       }else{
@@ -21,11 +21,11 @@ function booked() {
           exit;
       }
     } else {
-        alert('Please enter your Email address so that we can email you your train fare details')
+        alert('Please enter your Email address, so that we can email you your train fare details')
         let emailAskedAgain = prompt('Please enter your Email address')
         if (emailAskedAgain != 0) {
-            alert('We have mailed you the cheapest train ticket fares for the opted route')
-      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+            alert('We have mailed you, the cheapest train ticket fares for the opted route')
+      let deal = prompt('Please enter 1, to book you the best deal out of all of them')
       if(deal==1){
           alert('Your Ticket has been booked! Thank You for choosing us: Enjoy your Journey :)');
       }else{
@@ -38,12 +38,12 @@ function booked() {
 function trainPnrSearch() {
     let email = prompt('Please enter your Email address')
     if (email) {
-        alert('We have mailed you your PNR details')
+        alert('We have mailed you, your PNR details')
     } else {
-        alert('Please enter your Email address so that we can email you your PNR details')
+        alert('Please enter your Email address, so that we can email you your PNR details')
         let emailAskedAgain = prompt('Please enter your Email address')
         if (emailAskedAgain != 0) {
-            alert('We have mailed you your PNR details')
+            alert('We have mailed you, your PNR details')
         }
     }
 }
@@ -51,8 +51,8 @@ function trainPnrSearch() {
 function flightTicketBooked() {
     let email = prompt('Please enter your Email address')
     if (email) {
-        alert('We have mailed you the cheapest flight ticket fares for the opted route')
-      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+        alert('We have mailed you, the cheapest flight ticket fares for the opted route')
+      let deal = prompt('Please enter 1, to book you the best deal out of all of them')
       if(deal==1){
           alert('Your Ticket has been booked! Thank You for choosing us: Enjoy your Journey :)');
       }else{
@@ -60,17 +60,17 @@ function flightTicketBooked() {
           exit;
       }
     } else {
-        alert('Please enter your Email address so that we can email you your flight fare details')
+        alert('Please enter your Email address, so that we can email you your flight fare details')
         let emailAskedAgain = prompt('Please enter your Email address')
         if (emailAskedAgain != 0) {
-            alert('We have mailed you the cheapest train ticket fares for the opted route')
-      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+            alert('We have mailed you, the cheapest train ticket fares for the opted route')
+      let deal = prompt('Please enter 1, to book you the best deal out of all of them')
       if(deal==1){
           alert('Your Ticket has been booked! Thank You for choosing us: Enjoy your Journey :)');
       }else{
           alert('Thankyou for giving your time');
           exit;
-      }
+      },
         }
     }
 }
@@ -78,12 +78,12 @@ function flightPnrSearch() {
     let email = prompt('Please enter your Email address')
     console.log(email);
     if (email) {
-        alert('We have mailed you your PNR details')
+        alert('We have mailed you, your PNR details')
     } else {
-        alert('Please enter your Email address so that we can email you your PNR details')
+        alert('Please enter your Email address, so that we can email you your PNR details')
         let emailAskedAgain = prompt('Please enter your Email address')
         if (emailAskedAgain != 0) {
-            alert('We have mailed you your PNR details')
+            alert('We have mailed you, your PNR details')
         }
     }
 }
