73bcc99d949da81a95783b26b19420f7337e84f1
Avesh Singh <aveshzoop@gmail.com>
2022-04-14 13:52:16 +0530

    Search button updated

--- script.js
+++ script.js
@@ -9,21 +9,86 @@
 //     alert('This wheel is under construction')
 // })
 //Train Ticket booking
-function booked(){
-    alert('Your ticket has been booked! Happy Journey :)');
+function booked() {
+    let email = prompt('Please enter your Email address')
+    if (email) {
+        alert('We have mailed you the cheapest train ticket fares for the opted route')
+      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+      if(deal==1){
+          alert('We have booked your ticket! Enjoy your Journey :)');
+      }else{
+          alert('Thankyou for giving your time');
+          exit;
+      }
+    } else {
+        alert('Please enter your Email address so that we can email you your train fare details')
+        let emailAskedAgain = prompt('Please enter your Email address')
+        if (emailAskedAgain != 0) {
+            alert('We have mailed you the cheapest train ticket fares for the opted route')
+      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+      if(deal==1){
+          alert('We have booked your ticket! Enjoy your Journey :)');
+      }else{
+          alert('Thankyou for giving your time');
+          exit;
+      }
+        }
+    }
 }
-function trainPnrSearch(){
-    alert('We have mailed you your PNR details')
+function trainPnrSearch() {
+    let email = prompt('Please enter your Email address')
+    if (email) {
+        alert('We have mailed you your PNR details')
+    } else {
+        alert('Please enter your Email address so that we can email you your PNR details')
+        let emailAskedAgain = prompt('Please enter your Email address')
+        if (emailAskedAgain != 0) {
+            alert('We have mailed you your PNR details')
+        }
+    }
 }
 //Flight tickets
-function flightTicketBooked(){
-    alert('Your flight has been booked! Happy Flying :)')
+function flightTicketBooked() {
+    let email = prompt('Please enter your Email address')
+    if (email) {
+        alert('We have mailed you the cheapest flight ticket fares for the opted route')
+      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+      if(deal==1){
+          alert('We have booked your ticket! Enjoy your Journey :)');
+      }else{
+          alert('Thankyou for giving your time');
+          exit;
+      }
+    } else {
+        alert('Please enter your Email address so that we can email you your flight fare details')
+        let emailAskedAgain = prompt('Please enter your Email address')
+        if (emailAskedAgain != 0) {
+            alert('We have mailed you the cheapest train ticket fares for the opted route')
+      let deal = prompt('Please enter 1 to book you the best deal of all of them')
+      if(deal==1){
+          alert('We have booked your ticket! Enjoy your Journey :)');
+      }else{
+          alert('Thankyou for giving your time');
+          exit;
+      }
+        }
+    }
 }
-function flightPnrSearch(){
-    alert('We have mailed you your PNR details')
+function flightPnrSearch() {
+    let email = prompt('Please enter your Email address')
+    console.log(email);
+    if (email) {
+        alert('We have mailed you your PNR details')
+    } else {
+        alert('Please enter your Email address so that we can email you your PNR details')
+        let emailAskedAgain = prompt('Please enter your Email address')
+        if (emailAskedAgain != 0) {
+            alert('We have mailed you your PNR details')
+        }
+    }
 }
 
 //Contact Us
-function messageReceived(){
+function messageReceived() {
     alert('Thank You for contacting us')
 }
